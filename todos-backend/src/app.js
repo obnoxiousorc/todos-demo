b73477c64e4ddd6d1025892d891c89281202794e
Zed Jensen <zed@zayda.net>
2021-01-18 15:18:38 -0700

    Add SQLite database and API endpoints for basic todo functionality

--- todos-backend/src/app.js
+++ todos-backend/src/app.js
@@ -1,15 +1,55 @@
-const path = require('path')
-const express = require('express')
-const app = express()
-const port = 7000
+const express = require('express');
+const path = require('path');
+
+const Todo = require('./db/Todo.js');
+const sequelize = require('./db/Database.js');
+
+const port = 7000;
+const app = express();
+
+app.use(express.json());
 
 app.get('/api/test', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+app.get('/api/todos', async (req, res) => {
+  res.json(await Todo.findAll());
+});
+
+app.get('/api/todo/:id', async (req, res) => {
+  const todo = await Todo.findByPk(req.params.id);
+  if (!todo) {
+    res.status(404);
+    res.json({ error: 'No such todo!' });
+  } else {
+    res.json(todo);
+  }
+});
+
+app.post('/api/todo/new', async (req, res) => {
+  console.log(req.body);
+  try {
+    const newTodo = await Todo.create(req.body);
+    res.json(newTodo);
+  } catch (error) {
+    res.status(500);
+    res.json({ error: `Could not create todo: ${error}` });
+  }
+});
 
 // Look in ./public for anything not matching /api/
-app.use(/^\/(?!api\/)/, express.static(path.join(__dirname, 'public')))
+app.use(/^\/(?!api\/)/, express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`)
-})
+async function runServer() {
+  try {
+    await sequelize.authenticate();
+  } catch (error) {
+    console.error('Unable to connect to the database: ', error);
+  }
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+}
+
+runServer();
---
+++ todos-backend/src/db/Todo.js
@@ -0,0 +1,9 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('./Database');
+
+const Todo = sequelize.define('todo', {
+  name: DataTypes.STRING,
+  completed: DataTypes.BOOLEAN,
+});
+
+module.exports = Todo;
