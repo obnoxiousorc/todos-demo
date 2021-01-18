36d60456fbcddd69e1979beb20356f20c8f2cd89
Zed Jensen <zed@zayda.net>
2021-01-18 12:49:35 -0700

    Add basic static file routing, with an exception for /api/

--- todos-backend/app.js
+++ todos-backend/app.js
@@ -1,10 +1,14 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const port = 7000
 
-app.get('/', (req, res) => {
+app.get('/api/test', (req, res) => {
   res.send('Hello World!')
 })
+
+// Look in ./public for anything not matching /api/
+app.use(/^\/(?!api\/)/, express.static(path.join(__dirname, 'public')))
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
