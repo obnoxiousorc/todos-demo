8aa0b06d6d6305c457b405b78271eeb4c75db840
Zed Jensen <zed@zayda.net>
2021-01-19 15:05:34 -0700

    Add filter functionality to todos list

--- todos-frontend/src/App.js
+++ todos-frontend/src/App.js
@@ -3,11 +3,16 @@ import {
   Button,
   Container,
   Divider,
+  FormControl,
   Grid,
+  InputLabel,
   List,
   ListItem,
   ListItemText,
+  makeStyles,
+  MenuItem,
   Paper,
+  Select,
   Typography,
 } from '@material-ui/core';
 import { useEffect, useState } from 'react';
@@ -16,14 +21,34 @@ import { Add } from '@material-ui/icons';
 import TodoDialog from './components/TodoDialog';
 import Todo from './components/Todo';
 
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 200,
+  },
+}));
+
 function App() {
+  const styles = useStyles();
   // Used to force an update
+  const [filterSelectValue, setFilterSelectValue] = useState('all');
   const [dataVersion, setDataVersion] = useState(0);
   const [newTodoDialogOpen, setNewTodoDialogOpen] = useState(false);
   const { data, loading, error } = useAxios({
     url: 'todos',
     version: dataVersion,
   });
+
+  let todos;
+  if (data) {
+    if (filterSelectValue === 'uncompleted') {
+      todos = data.filter((todo) => !todo.completed);
+    } else if (filterSelectValue === 'completed') {
+      todos = data.filter((todo) => todo.completed);
+    } else {
+      todos = data;
+    }
+  }
 
   return (
     <div className="App">
@@ -38,21 +63,37 @@ function App() {
             <Typography variant="h4">Todos</Typography>
           </Grid>
           <Grid item>
+            <FormControl variant="outlined" className={styles.formControl}>
+              <InputLabel id="filterLabel">Filter</InputLabel>
+              <Select
+                labelId="filterLabel"
+                id="filterSelect"
+                value={filterSelectValue}
+                onChange={(e) => setFilterSelectValue(e.target.value)}
+                label="Filter"
+              >
+                <MenuItem value="all">All</MenuItem>
+                <MenuItem value="uncompleted">Uncompleted</MenuItem>
+                <MenuItem value="completed">Completed</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item>
             <Paper>
               <List style={{ maxHeight: '60vh', overflow: 'auto' }}>
-                {!data || data.length === 0 ? (
+                {!todos || todos.length === 0 ? (
                   <ListItem>
                     <ListItemText>No todos yet!</ListItemText>
                   </ListItem>
                 ) : (
-                  data.map((todo, i) => (
+                  todos.map((todo, i) => (
                     <>
                       <Todo
                         key={i}
                         todo={todo}
                         refresh={() => setDataVersion(dataVersion + 1)}
                       />
-                      {i < data.length - 1 ? <Divider /> : null}
+                      {i < todos.length - 1 ? <Divider /> : null}
                     </>
                   ))
                 )}
